113fda5040c0dd562d1c40672bd7bc7f3776a013
neil.drinkall <Neil.Drinkall@globalrelay.net>
2022-11-28 17:45:55 +0000

    Getting ready

---
+++ AoC2022/Days/Day1.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC2022.Days;
+
+public static class Day1
+{
+    public static async Task Run()
+    {
+        var input = await InputGetter.GetFromLinesAsString(1);
+        await PartOne(input);
+        await PartTwo(input);
+    }
+
+    private static async Task PartOne(string[] input)
+    {
+
+    }
+
+    private static async Task PartTwo(string[] input)
+    {
+
+    }
+}
---
+++ AoC2022/Program.cs
@@ -0,0 +1,4 @@
+﻿global using AoC2022.Utils;
+using AoC2022.Days;
+
+await Day1.Run();
