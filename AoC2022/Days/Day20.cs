292c12d881490322f6b88823f7b91b497be386bd
neil.drinkall <Neil.Drinkall@globalrelay.net>
2022-12-20 17:08:40 +0000

    Day20

---
+++ AoC2022/Days/Day20.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace AoC2022.Days;
+
+public static class Day20
+{
+
+    public static async Task Run()
+    {
+        var input = await InputGetter.GetFromLinesAsLong(20);
+
+        var part1 = await Run(input, 1, 1);
+        Console.WriteLine($"Part One - {part1}");
+
+        var part2 = await Run(input, 10, 811589153);
+        Console.WriteLine($"Part Two - {part2}");
+    }
+
+    private static async Task<long> Run(long[] input, int mixCount, int decryptionKey)
+    {        
+        var originalNumbers = input.Select((i, idx) => (idx, i * decryptionKey)).ToList(); 
+        var numbers = new List<(int index, long value)>(originalNumbers);
+        
+        for (var i = 0; i < mixCount; i++)
+        {
+            foreach (var (index, value) in originalNumbers)
+            {
+                var pos = numbers.IndexOf((index, value));
+                var newPos = (int)((pos + value) % (numbers.Count - 1));
+                if (newPos < 0)
+                {
+                    newPos += numbers.Count - 1;
+                }
+
+                numbers.RemoveAt(pos);
+                numbers.Insert(newPos, (index, value));
+            }
+        }
+
+        var zeroIdx = numbers.FindIndex(i => i.value == 0);
+        var result = 
+            numbers[(1000 + zeroIdx) % numbers.Count].value +
+            numbers[(2000 + zeroIdx) % numbers.Count].value +
+            numbers[(3000 + zeroIdx) % numbers.Count].value;
+
+
+        return result;
+
+    }  
+
+ }
--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -1,4 +1,4 @@
 ﻿global using AoC2022.Utils;
 using AoC2022.Days;
 
-await Day19.Run();
+await Day20.Run();
